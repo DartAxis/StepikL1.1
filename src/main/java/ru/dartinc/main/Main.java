fe740e8478ea317304158cc847eb2465c670225e
DartAxis <Filatyev.a.a@gmail.com>
2020-11-11 15:24:56 +0300

    исправление работы с ресурсами

--- src/main/java/ru/dartinc/main/Main.java
+++ src/main/java/ru/dartinc/main/Main.java
@@ -4,15 +4,23 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import ru.dartinc.servlets.AllRequestServlet;
+import ru.dartinc.servlets.MirrorServlet;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         AllRequestServlet frontend = new AllRequestServlet();
+        MirrorServlet mirrorServlet = new MirrorServlet();
+
+
         Server server = new Server(8085);
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         server.setHandler(context);
+
         context.addServlet(new ServletHolder(frontend),"/*");
+        context.addServlet(new ServletHolder(mirrorServlet),"/mirror");
+
         server.start();
+        System.out.println("Server started");
         server.join();
     }
 }
